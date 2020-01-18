9093e352a0e9f259c84189850d83ee5d31529cd0
William M <wmooney1984@gmail.com>
2020-01-17 20:25:00 -0500

    Added Integration & Unit Tests

--- meadowlark.js
+++ meadowlark.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const expressHandlebars = require('express-handlebars');
-const fortune = require('./lib/fortune');
+
+const handlers = require('./lib/handlers');
 
 const app = express();
 
@@ -13,32 +14,27 @@ app.engine(
 );
 app.set('view engine', 'handlebars');
 
-app.use(express.static(__dirname + '/public'));
-
 const port = process.env.PORT || 3000;
 
-app.get('/', (req, res) => res.render('home'));
+app.use(express.static(__dirname + '/public'));
 
-app.get('/about', (req, res) => {
-  res.render('about', { fortune: fortune.getFortune() });
-});
+app.get('/', handlers.home);
+
+app.get('/about', handlers.about);
 
 // custom 404 page
-app.use((req, res) => {
-  res.status(404);
-  res.render('404');
-});
+app.use(handlers.notFound);
 
 // custom 500 page
-app.use((err, req, res, next) => {
-  console.error(err.message);
-  res.status(500);
-  res.render('500');
-});
+app.use(handlers.serverError);
 
-app.listen(port, () =>
-  console.log(
-    `Express started on http://localhost:${port}; ` +
-      `press Ctrl-C to terminate.`
-  )
-);
+if (require.main === module) {
+  app.listen(port, () => {
+    console.log(
+      `Express started on http://localhost:${port}` +
+        '; press Ctrl-C to terminate.'
+    );
+  });
+} else {
+  module.exports = app;
+}
