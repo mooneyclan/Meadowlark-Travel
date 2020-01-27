9df9792d4049229cc8cbe36d5a3dbed8149c950c
William M <wmooney1984@gmail.com>
2020-01-26 22:07:41 -0500

    Form Added

--- meadowlark.js
+++ meadowlark.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const expressHandlebars = require('express-handlebars');
+const bodyParser = require('body-parser');
+const multiparty = require('multiparty');
 
 const handlers = require('./lib/handlers');
 const weatherMiddlware = require('./lib/middleware/weather');
@@ -22,6 +24,9 @@ app.engine(
 );
 app.set('view engine', 'handlebars');
 
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
 const port = process.env.PORT || 3000;
 
 app.use(express.static(__dirname + '/public'));
@@ -29,7 +34,41 @@ app.use(express.static(__dirname + '/public'));
 app.use(weatherMiddlware);
 
 app.get('/', handlers.home);
-app.get('/section-test', handlers.sectionTest);
+
+// handlers for browser-based form submission
+app.get('/newsletter-signup', handlers.newsletterSignup);
+app.post('/newsletter-signup/process', handlers.newsletterSignupProcess);
+app.get('/newsletter-signup/thank-you', handlers.newsletterSignupThankYou);
+
+// handlers for fetch/JSON form submission
+app.get('/newsletter', handlers.newsletter);
+app.post('/api/newsletter-signup', handlers.api.newsletterSignup);
+
+// vacation photo contest
+app.get('/contest/vacation-photo', handlers.vacationPhotoContest);
+app.get('/contest/vacation-photo-ajax', handlers.vacationPhotoContestAjax);
+app.post('/contest/vacation-photo/:year/:month', (req, res) => {
+  const form = new multiparty.Form();
+  form.parse(req, (err, fields, files) => {
+    if (err)
+      return handlers.vacationPhotoContestProcessError(req, res, err.message);
+    console.log('got fields: ', fields);
+    console.log('and files: ', files);
+    handlers.vacationPhotoContestProcess(req, res, fields, files);
+  });
+});
+app.get(
+  '/contest/vacation-photo-thank-you',
+  handlers.vacationPhotoContestProcessThankYou
+);
+app.post('/api/vacation-photo-contest/:year/:month', (req, res) => {
+  const form = new multiparty.Form();
+  form.parse(req, (err, fields, files) => {
+    if (err)
+      return handlers.api.vacationPhotoContestError(req, res, err.message);
+    handlers.api.vacationPhotoContest(req, res, fields, files);
+  });
+});
 
 app.use(handlers.notFound);
 app.use(handlers.serverError);
